47f8ebca5e76afcbc1bd465e1f836877545a1594
clayf <839322915@qq.com>
2024-02-27 21:44:35 +0800

    删除sqltoy，经过多方面衡量选择使用mybatis-plus

--- src/test/java/com/f/core/test/LogTest.java
+++ src/test/java/com/f/core/test/LogTest.java
@@ -1,14 +1,20 @@
 package com.f.core.test;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 /**
  * created by f at 8/19/21 23:46
  */
 public class LogTest {
+
     public static void main(String[] args) {
-        Logger logger  = LoggerFactory.getLogger(LogTest.class);
-        logger.info("hello");
+//        String s = """
+//        #EXTINF:1.250000,
+//        https://ts2.hjf5e.com/hjstore/video/20231209/bf60a1da8f92fcd33c4313a3e18235e5/6782062So6A8dbs_i%s.ts""";
+//        for (int i = 0; i <= 1000; i++) {
+//            System.out.println(String.format(s, i));
+//        }
+
+
     }
+
+
 }
